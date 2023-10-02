7b07975f61470a6a6271e13c4b161c269d467edf
SpaSinghOut <spartaksingh95@gmail.com>
2023-10-01 22:01:54 -0500

    1

--- site/src/jsMain/kotlin/com/spartanlabs/MyApp.kt
+++ site/src/jsMain/kotlin/com/spartanlabs/MyApp.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.compose.ui.modifiers.fontFamily
 import com.varabyte.kobweb.compose.ui.modifiers.minHeight
 import com.varabyte.kobweb.core.App
 import com.varabyte.kobweb.silk.SilkApp
@@ -13,11 +14,23 @@ import com.varabyte.kobweb.silk.components.style.toModifier
 import com.varabyte.kobweb.silk.components.text.SpanText
 import com.varabyte.kobweb.silk.init.InitSilk
 import com.varabyte.kobweb.silk.init.InitSilkContext
+import com.varabyte.kobweb.silk.init.registerBaseStyle
+import com.varabyte.kobweb.silk.theme.colors.ColorMode
+import kotlinx.browser.localStorage
 import org.jetbrains.compose.web.css.vh
+
+private const val COLOR_MODE_KEY = "site:colorMode"
 
 @InitSilk
 fun initSilk(ctx: InitSilkContext) {
-    // Configure silk here
+    ctx.config.initialColorMode = localStorage.getItem(COLOR_MODE_KEY)?.let { ColorMode.valueOf(it) } ?: ColorMode.LIGHT
+
+    ctx.stylesheet.registerBaseStyle("body") {
+        Modifier.fontFamily(
+            "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Oxygen", "Ubuntu",
+            "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "sans-serif"
+        )
+    }
 }
 
 @App
@@ -27,5 +40,5 @@ fun MyApp(content: @Composable () -> Unit) {
         Surface(SmoothColorStyle.toModifier().minHeight(100.vh)) {
             content()
         }
-        }
+    }
 }
