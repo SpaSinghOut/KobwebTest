dd5c783b578077a82e37f65502cc8d8d2ee0b552
SpaSinghOut <spartaksingh95@gmail.com>
2023-10-01 21:46:20 -0500

    0

--- site/src/jsMain/kotlin/com/spartanlabs/pages/Index.kt
+++ site/src/jsMain/kotlin/com/spartanlabs/pages/Index.kt
@@ -1,18 +1,27 @@
 package com.spartanlabs.pages
 
 import androidx.compose.runtime.*
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.foundation.layout.Row
+import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
 import com.varabyte.kobweb.core.Page
+import com.varabyte.kobweb.silk.SilkApp
 import com.varabyte.kobweb.silk.components.text.SpanText
 
 @Page
 @Composable
 fun HomePage() {
     // TODO: Add content here
-    //println("Hello World!")
-    Column(Modifier.fillMaxSize()) {
+    println("Hello World!")
+    Column(Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,){
+        Row(verticalAlignment = Alignment.CenterVertically,){
+            SpanText("Hello, world!")
+        }
         SpanText("Hello, world!")
     }
 }
